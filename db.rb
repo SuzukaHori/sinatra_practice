eb67fcaad100770eec6accf4e45bf1d8c3d3ff64
SuzukaHori <horikoshi.suzuka@gmail.com>
2023-07-21 16:44:44 +0900

    jsonファイルをDBに置き換えた
    データ保存先をDBに変更しました。

--- db.rb
+++ db.rb
@@ -1,10 +1,32 @@
 # frozen_string_literal: true
 
 require 'json'
+require 'pg'
+
+DBNAME = 'memosdata'
+
+def connect_database
+  PG.connect(dbname: DBNAME)
+end
+
+def execute_sql(params, sql)
+  if params[:name]
+    name = params[:name]
+    text = params[:text]
+  end
+  id = params[:id]
+  value_to_replace_bind_variable = [name, text, id].compact
+  connection = connect_database
+  connection.exec_params(sql, value_to_replace_bind_variable)
+end
 
 def read_all_memos
-  original_data = File.read('db.json')
-  JSON.parse(original_data, symbolize_names: true)
+  connection = connect_database
+  all_memos = connection.exec('SELECT * FROM memos;')
+  all_memos.map do |memo|
+    memo.store('id', memo['id'].to_i)
+    memo.transform_keys(&:to_sym)
+  end
 end
 
 def find_memo(id)
@@ -12,35 +34,32 @@ def find_memo(id)
   memos.find { |memo| memo[:id] == id }
 end
 
-def write_memos(memos)
-  File.open('db.json', 'w') { |file| file.write(JSON.pretty_generate(memos)) }
+def delete_memo(id)
+  sql = <<~SQL
+    DELETE FROM memos
+    WHERE id = $1;
+  SQL
+  params = { id: id }
+  execute_sql(params, sql)
 end
 
-def delete_memo(id)
-  old_memos = read_all_memos
-  new_memos = old_memos.reject { |memo| memo[:id] == id }
-  write_memos(new_memos)
-end
+delete_memo(16)
 
 def edit_memo(params)
+  sql = <<~SQL
+    UPDATE memos
+    SET name = $1::VARCHAR, text = $2::VARCHAR
+    WHERE id =  $3::int;
+  SQL
   symbolized_params = params.transform_keys(&:to_sym)
-  memos = read_all_memos
-  memo = memos.find { |x| x[:id] == symbolized_params[:id].to_i }
-  memo[:name] = symbolized_params[:name]
-  memo[:text] = symbolized_params[:text]
-  write_memos(memos)
-end
-
-def generate_id
-  memos = read_all_memos
-  last_id = memos.empty? ? 0 : memos[-1][:id]
-  last_id + 1
+  execute_sql(symbolized_params, sql)
 end
 
 def add_memo(params)
+  sql = <<~SQL
+    INSERT INTO memos(name, text)
+    VALUES ($1::VARCHAR, $2::VARCHAR);
+  SQL
   symbolized_params = params.transform_keys(&:to_sym)
-  id = generate_id
-  memos = read_all_memos
-  memos.push(symbolized_params.slice(:name, :text).merge(id:))
-  write_memos(memos)
+  execute_sql(symbolized_params, sql)
 end
